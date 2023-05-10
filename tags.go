43901d21c3baa226ac53aa020f92886f1ff2e2c0
Taras Shumeiko <shumeyko@gmail.com>
2023-05-10 14:24:16 +0300

    add ForEach to Tags struct

--- tags.go
+++ tags.go
@@ -24,3 +24,18 @@ func (t *Tags) ToMap() map[string]interface{} {
 	}
 	return out
 }
+
+func (t *Tags) ForEach(handle func(string, interface{})) {
+	out := make(map[string]struct{})
+	if t == nil {
+		return
+	}
+	entry := t
+	for entry != nil {
+		if _, ok := out[entry.key]; !ok {
+			out[entry.key] = struct{}{}
+			handle(entry.key, entry.val)
+		}
+		entry = entry.parent
+	}
+}
